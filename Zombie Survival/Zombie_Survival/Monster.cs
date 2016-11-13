1184d8972a68ae60d91884de693412c27372fa25
David William Fitzgerald <Dankerprouduct2@gmail.com>
2016-11-13 14:14:50 -0500

    Current code

---
+++ Zombie Survival/Zombie_Survival/Monster.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content; 
+
+namespace Zombie_Survival
+{
+    public class Monster
+    {
+        private string name;
+        private int health;
+        private int speed;
+        private float damage; 
+
+        public Monster(ref MonsterSpawner monsterSpawner)
+        {
+            this.name = monsterSpawner.GetName();
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            MonsterProperties[] monsterProperties = content.Load<Zombie_Survival.MonsterProperties[]>("Monsters");
+            for (int i = 0; i < monsterProperties.Length; i++)
+            {
+                if (monsterProperties[i].name == this.name)
+                {
+                    health = monsterProperties[i].health;
+                    speed = monsterProperties[i].speed;
+                    damage = monsterProperties[i].damage; 
+                }
+            }
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
---
+++ Zombie Survival/Zombie_Survival/MonsterSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content; 
+
+namespace Zombie_Survival
+{
+    public class MonsterSpawner
+    {
+
+        private string name;
+        
+        public MonsterSpawner(string name)
+        {
+            this.name = name;
+        }
+
+        public Monster SpawnMonster()
+        {
+            MonsterSpawner monsterSpawner = this;
+            return new Monster(ref monsterSpawner); 
+        }
+
+        public string GetName()
+        {
+            return name; 
+        }
+    }
+}
